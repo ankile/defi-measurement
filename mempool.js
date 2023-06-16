1bd5db5f5d78a8a1ddfd381cfc56c4c478790f03
ankile <lars.ankile@gmail.com>
2023-06-16 14:10:14 -0400

    Started script that saves mempool transactions

---
+++ mempool.js
@@ -0,0 +1,27 @@
+var ethers = require("ethers");
+var url =
+  "wss://powerful-quiet-tree.discover.quiknode.pro/a57622ac783ac8de95c780597f3e6b5cc9d609ef/";
+
+var init = function () {
+  var customWsProvider = new ethers.providers.WebSocketProvider(url);
+
+  customWsProvider.on("pending", (tx) => {
+    customWsProvider.getTransaction(tx).then(function (transaction) {
+      console.log(transaction);
+    });
+  });
+
+  customWsProvider._websocket.on("error", async () => {
+    console.log(`Unable to connect to ${ep.subdomain} retrying in 3s...`);
+    setTimeout(init, 3000);
+  });
+  customWsProvider._websocket.on("close", async (code) => {
+    console.log(
+      `Connection lost with code ${code}! Attempting reconnect in 3s...`
+    );
+    customWsProvider._websocket.terminate();
+    setTimeout(init, 3000);
+  });
+};
+
+init();

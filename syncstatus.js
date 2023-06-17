b18351daa438062cce7c5757a397850d7b39db3d
ankile <lars.ankile@gmail.com>
2023-06-17 11:33:01 -0400

    Update the status getter

--- syncstatus.js
+++ syncstatus.js
@@ -3,10 +3,8 @@ const ethers = require("ethers");
 // Make an HTTP Provider
 const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
 
-const signer = provider.getSigner();
-
 async function getSyncStatus() {
-  const syncInfo = await signer.send("eth_syncing");
+  const syncInfo = await provider.send("eth_syncing");
   return syncInfo;
 }
 

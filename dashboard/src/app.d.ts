786c7deb45dffffd9cbe967b9269ff0e4a98a5be
ankile <lars.ankile@gmail.com>
2023-06-21 21:27:35 -0400

    Smallc hanges

---
+++ dashboard/src/app.d.ts
@@ -0,0 +1,14 @@
+// See https://kit.svelte.dev/docs/types#app
+/// <reference types="svelte-adapter-azure-swa" />
+
+// for information about these interfaces
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface Platform {}
+	}
+}
+
+export {};

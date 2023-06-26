20e2bf65dcb223eb0f561905c768ed45eec63e99
ankile <lars.ankile@gmail.com>
2023-06-26 19:18:55 -0400

    Fix an issue in the charts page

--- dashboard/src/lib/prisma/index.ts
+++ dashboard/src/lib/prisma/index.ts
@@ -1,13 +1,33 @@
 import { PrismaClient } from '@prisma/client';
-import { getServerSession } from '$app/env';
 
-let prismaClient: PrismaClient | null = null;
+// let prismaClient: PrismaClient | null = null;
 
-export function getPrismaClient() {
-	if (getServerSession()) {
-		if (!prismaClient) {
-			prismaClient = new PrismaClient();
-		}
-		return prismaClient;
-	}
+// export function getPrismaClient() {
+// 	if (getServerSession()) {
+// 		if (!prismaClient) {
+// 			prismaClient = new PrismaClient();
+// 		}
+// 		return prismaClient;
+// 	}
+// }
+
+const prisma = new PrismaClient();
+
+export async function getTransactionCounts() {
+	const queryResult: { hour: Date; transactioncount: Number }[] = await prisma.$queryRaw`
+    SELECT 
+      DATE_TRUNC('hour', first_seen) as hour,
+      COUNT(hash) as transactionCount
+    FROM
+      mempool_transactions
+    GROUP BY
+      DATE_TRUNC('hour', first_seen)
+    ORDER BY
+      DATE_TRUNC('hour', first_seen);
+  `;
+
+	return queryResult.map((row) => ({
+		hour: row.hour,
+		transactionCount: row.transactioncount,
+	}));
 }
--- dashboard/src/routes/chart/+page.server.ts
+++ dashboard/src/routes/chart/+page.server.ts
@@ -1,41 +1,15 @@
+import { getTransactionCounts } from '$lib/prisma';
 import type { PageServerLoad } from './$types';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-type TransactionCount = {
-	hour: Date;
-	transactionCount: number;
-};
 
 export const load: PageServerLoad = async () => {
 	console.log('In load (yeet!)');
 
-	const transactionCounts: TransactionCount[] = await prisma.$queryRaw`
-    SELECT 
-      DATE_TRUNC('hour', first_seen) as hour,
-      COUNT(hash) as transactionCount
-    FROM
-      mempool_transactions
-    GROUP BY
-      DATE_TRUNC('hour', first_seen)
-    ORDER BY
-      DATE_TRUNC('hour', first_seen);
-  `.then((result) =>
-		result.map((row) => ({
-			hour: row.hour,
-			transactionCount: row.transactioncount,
-		})),
-	);
-
-	console.log('Ran query, got results', transactionCounts);
+	const transactionCounts = await getTransactionCounts();
 
 	// Convert big int to number
 	transactionCounts.forEach((row) => {
 		row.transactionCount = Number(row.transactionCount);
 	});
-
-	console.log('Converted big int to number returning');
 
 	return { transactionCounts };
 };

86796583671f883d8a5a58108ae2020a9faa2d7b
ankile <lars.ankile@gmail.com>
2023-07-06 12:04:52 -0400

    Make histogram nice

--- dashboard/src/routes/+layout.svelte
+++ dashboard/src/routes/+layout.svelte
@@ -27,6 +27,7 @@
 	<li><a href="/mempool-share">Mempool Share</a></li>
 	<li><a href="/permutations">Transaction Order Permutations</a></li>
 	<li><a href="/swaps-in-block">Swaps in Blocks per Pool</a></li>
+	<li><a href="/three-swap-blocks">Share of Swap Order for 3-Swap Blocks</a></li>
 </ul>
 
 <body data-sveltekit-preload-data="hover">
--- dashboard/src/routes/swaps-in-block/+page.svelte
+++ dashboard/src/routes/swaps-in-block/+page.svelte
@@ -7,6 +7,29 @@
 
 	const { count, frequency } = data;
 
+	const total = frequency.reduce((a, b) => a + b, 0);
+
+	function dynamicToFixed(value: number) {
+		let absoluteValue = Math.abs(value);
+		if (absoluteValue >= 1) {
+			return value.toFixed(0);
+		} else if (absoluteValue >= 0.1) {
+			return value.toFixed(1);
+		} else if (absoluteValue >= 0.01) {
+			return value.toFixed(2);
+		} else if (absoluteValue >= 0.001) {
+			return value.toFixed(3);
+		} else if (absoluteValue >= 0.0001) {
+			return value.toFixed(4);
+		} else if (absoluteValue >= 0.00001) {
+			return value.toFixed(5);
+		} else if (absoluteValue >= 0.000001) {
+			return value.toFixed(6);
+		} else {
+			return value.toFixed(0);
+		}
+	}
+
 	let scale = 'linear';
 	let chart: Highcharts.Chart;
 
@@ -33,11 +56,20 @@
 				},
 				crosshair: true,
 			},
-			yAxis: {
-				title: {
-					text: 'Frequency',
+			tooltip: {
+				formatter: function () {
+					const count = Math.round(this.y * total);
+					return `Percentage: ${(this.y * 100).toFixed(2)}%<br>Absolute Count: ${count}`;
 				},
+			},
+			yAxis: {
 				type: scale,
+				title: { text: 'Percentage of Total' },
+				labels: {
+					formatter: function () {
+						return dynamicToFixed(this.value * 100) + '%';
+					},
+				},
 			},
 			plotOptions: {
 				bar: {
@@ -48,10 +80,10 @@
 			},
 			series: [
 				{
-					name: 'Transactions per hour',
+					name: 'Swaps per block',
 					showInLegend: false,
 					type: 'column',
-					data: frequency,
+					data: frequency.map((item) => item / total),
 					// Make a nice color that is not too dark, e.g., purple
 					color: 'rgba(75, 0, 130, 1)',
 				},
@@ -85,4 +117,6 @@
 
 <div id="chart-container" />
 
-<button on:click={changeType}>Change to: {scale === 'logarithmic' ? 'Linear' : 'Logarithmic'}</button>
+<button on:click={changeType}
+	>Change to: {scale === 'logarithmic' ? 'Linear' : 'Logarithmic'}</button
+>
